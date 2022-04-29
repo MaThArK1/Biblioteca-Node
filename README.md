898a316aae12d74a90e698d51867ea82113a5062
MaThArK1 <matheus_barucho@hotmail.com>
2022-04-29 18:06:00 -0300

    Adicionado a lib fs (then, e catch)

---
+++ README.md
@@ -0,0 +1,3 @@
+NODEjs - Criando a primeira biblioteca.
+
+Projeto de exemplo do curso: NodeJS: criando sua primeira biblioteca.
--- lib-markdown/index.js
+++ lib-markdown/index.js
@@ -1,12 +1,47 @@
-//const chalk = import('chalk')
+//const chalk = require('chalk')
 import chalk from "chalk";
+import fs from "fs";
 
-console.log(chalk.blue('Vamos começar!'));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto (string) {
-    return string;
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'Não foi possível encontrar o arquivo!'));
 }
 
-console.log(texto(paragrafo));
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch((erro) => trataErro(erro)) ;
+    
+}
+
+/* function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if (erro) {
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    })
+} */
+
+pegaArquivo('./arquivos/texto1.md');
+
+//const chalk = require('chalk');
+//const fs = require('fs');
+//
+////encadear métodos para colorir texto, cor de fundo e texto em negrito
+//console.log(chalk.blue.bgWhite.bold('Alura'));
+//
+////receber múltiplos argumentos
+//console.log(chalk.blue('curso', 'de', 'NodeJS'));
+//
+////métodos aninhados
+//console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
+//
+//// uso de template strings e placeholders
+//console.log(`
+//CPU: ${chalk.red('90%')}
+//RAM: ${chalk.green('40%')}
+//DISK: ${chalk.yellow('70%')}
+//`);
